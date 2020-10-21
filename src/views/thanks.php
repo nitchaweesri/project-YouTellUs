b7e4d811ec66dc3791a444ca2e369458aebb6518
nitchawee <nitchawee.s@gmail.com>
2020-10-21 10:56:59 +0700

    add BG on Thanks

--- src/views/thanks.php
+++ src/views/thanks.php
@@ -2,10 +2,21 @@
 <html lang="en">
 <?php include 'header.php';?>
 
+<style>
+    #main {
+        background-image: url("public/img/bg-thanks.png");
+        background-repeat: no-repeat;
+        background-size: 320px;
+        height: 100vh;
+        background-position-x: right;
+        background-position-y: center;
+    }
+</style>
+
 <body>
     <?php include 'navbar.php';?>
     <div style="height:80px"></div>
-    <div class="container mb-4">
+    <div class="container mb-4" id="main">
         <div class="row mt-4">
             <div class="col">
                 <h2 class="text-primary text-center">ขอบคุณค่ะ</h2>

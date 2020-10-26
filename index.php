afaf2724c9133720963a230c7682f203a224cd71
Matsafik <matsafik.be@inet.co.th>
2020-10-26 11:36:51 +0700

    no htacees

--- index.php
+++ index.php
@@ -1,20 +1,56 @@
 <?php
-	$url = "https://devytuapp.tellvoice.com/TVSSCRAWLER3/youtellus/webhook.php";
-	
-	$ch = curl_init( $url );
-	# Setup request to send json via POST.
-	
-	$ParamArr = array("data"=> "FIK TEST" );
-	$payload = json_encode( $ParamArr );
-	curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
-	curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-	# Return response instead of printing.
-	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-	# Send request.
-	$result = curl_exec($ch);
-	curl_close($ch);
-	# Print response.
-    echo "<pre>$result</pre>";
-    
-     
-?>
+
+// $_GET['path'] = isset($_GET['path']) ? $_GET['path'] : "";
+// // echo $_GET['path'];
+
+
+// // if (strpos( $_GET['path'], '/hello/') === 0) {
+// //     $names = preg_replace('|^\/hello\/|', '',  $_GET['path']);
+// //     $names = str_replace('/','-', $names);
+// //     // return "/hello/$names";
+// // //   }
+// //     $_GET['path'] = $names;
+
+// switch ($_GET['path']) {
+//     case '' :
+//         require 'src/views/condition.php';
+//         break;
+//     case 'menu' :
+//         require 'src/views/menu.php';
+//         break;
+//     case 'form1' :
+//         require 'src/views/form1.php';
+//         break;
+//     case 'form1-submit' :
+//         require 'src/views/form1.php';
+//         break;
+//     case 'form2' :
+//         require 'src/views/form2.php';
+//         break;
+//     case 'form2-submit' :
+//         require 'src/views/form2.php';
+//         break;
+//     case 'data1' :
+//         require 'src/controller/data1.php';
+//         break;
+//     case 'store' :
+//         require 'src/views/store.php';
+//         break;
+//     case 'form3' :
+//         require 'src/views/form3.php';
+//         break;
+//     case 'form3-submit' :
+//         require 'src/views/form3.php';
+//         break;
+//     case 'thanks' :
+//         require 'src/views/thanks.php';
+//         break;
+//     default:
+//         http_response_code(404);
+//         require 'src/views/condition.php';
+//         break;
+// }
+
+
+include 'src/views/condition.php';
+

f5650ad3df81f4695f432aba4c5d353a488edd7b
Matsafik <matsafik.be@inet.co.th>
2020-11-11 11:18:22 +0700

    2

--- resources/views/verify.php
+++ resources/views/verify.php
@@ -73,13 +73,13 @@ $(function () {
     $('#makesession').on('click', function () {
         $.ajax({
             type: "POST",
-            url: './controllers/sessionWrite.php',
+            url: 'controllers/sessionWrite.php',
             data:{"name": "countStart","value":new Date().getTime()}
             // data: {sessionJson: { countStart :'countStartvalue1' , countStart1: 'countStar1tvalue1'}}
         }); 
         $.ajax({
             type: "POST",
-            url: './controllers/sessionWrite.php?',
+            url: 'controllers/sessionWrite.php?',
             data:{"name": "phoneNo","value":$('#tell').val()}
             // data: {sessionJson: { countStart :'countStartvalue1' , countStart1: 'countStar1tvalue1'}}
         }); 
@@ -88,7 +88,7 @@ $(function () {
         if (countMistake == '') {
             $.ajax({
             type: "POST",
-            url: './controllers/sessionWrite.php?name=countMistake&value=0',
+            url: 'controllers/sessionWrite.php?name=countMistake&value=0',
             data:{"name": "countMistake","value":"0"}
             // data: {sessionJson: { countStart :'countStartvalue1' , countStart1: 'countStar1tvalue1'}}
         }); 

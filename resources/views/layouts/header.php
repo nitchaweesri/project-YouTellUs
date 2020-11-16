5d17265ebb4bf1ecd350ff4f68e5cdfb1b823fe0
nitchawee <nitchawee.s@gmail.com>
2020-11-16 12:07:37 +0700

    update nav

--- resources/views/layouts/header.php
+++ resources/views/layouts/header.php
@@ -9,7 +9,7 @@ SESSION CountMistake is : <?php echo isset($_SESSION['countMistake'])? $_SESSION
 <nav class="navbar navbar-light  fixed-top" style="background-color:#ffffff; box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.1);">
 	<div class="container">
     	<div class="logo mr-auto ml-lg-5">
-    		<a href="index.php?page=2"><img src="public/img/icon.png" width="" height="40" class="d-inline-block align-top" alt="" loading="lazy"></a>
+    		<a href="<?php echo isset($_SESSION['pending'])? "index.php?page=menuupload": "index.php?page=2" ?>"><img src="public/img/icon.png" width="" height="40" class="d-inline-block align-top" alt="" loading="lazy"></a>
     	</div>
     	<div class="lang-btn mr-lg-5">
     		<a <?php echo (@$lang == 'th') ? 'class="se"':'' ; ?> onclick="setGetParameter('lang','th')">ไทย</a>&nbsp; &Iota;&nbsp;
